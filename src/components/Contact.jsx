a9dca0348117751e75af936c4cb83077ec971dde
charantatineni <charantatineni11@gmail.com>
2025-03-11 16:32:42 -0400

    Moved EmailJS configuration into Contact component and updated contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_28ztq3w',  // EmailJS Service ID
+        'template_afut257', // EmailJS Template ID
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Charan",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "charantatineni@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'Q6OrgXKIat1CzV6ZF'  // EmailJS Public Key
       )
       .then(
         () => {
@@ -87,7 +87,7 @@ const Contact = () => {
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
